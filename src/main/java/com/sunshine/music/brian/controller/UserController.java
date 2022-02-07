d97f1c3bf542f9764af63645733e6244c3a42a77
Sven <sven199109@gmail.com>
2022-02-07 22:29:20 +0800

    增加logback配置

--- src/main/java/com/sunshine/music/brian/controller/UserController.java
+++ src/main/java/com/sunshine/music/brian/controller/UserController.java
@@ -1,22 +1,37 @@
 package com.sunshine.music.brian.controller;
 
+import com.sunshine.music.brian.dao.entity.UserEntity;
 import com.sunshine.music.brian.service.UserService;
 import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.winterframework.core.support.ApiResponse;
+
+import java.util.List;
 
 /**
  * @author sven
  * Created on 2022/1/28 2:26 下午
  */
+@Slf4j
 @RestController
 public class UserController {
     @Resource
     private UserService userService;
 
     @RequestMapping("/findByUsername")
-    public Object findByUsername(@RequestParam String username) {
-        return userService.findByUsername(username);
+    public ApiResponse<List<UserEntity>> findByUsername(@RequestParam String username) {
+        ApiResponse<List<UserEntity>> response = new ApiResponse<>();
+        response.setData(userService.findByUsername(username));
+        response.setCode(200);
+        return response;
+    }
+
+    @RequestMapping("/testErrorLog")
+    public ApiResponse<?> testErrorLog() {
+        log.error("TestFor Error: {}", "错误信息");
+        return new ApiResponse<>();
     }
 }
